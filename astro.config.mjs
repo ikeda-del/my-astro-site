d67205fbd0ecaded272f9fba8af09dd5c200549f
ikeda <ikeda@otn.jp>
2026-06-03 11:38:44 +0900

    初回コミット

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite'; // 1. インポートを追加
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()], // 2. plugins に追加
+  },
+});
